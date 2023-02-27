6a04d95b0ba953c4c5be430721d12ff9af305fa8
joe <63621767+joehutcheson@users.noreply.github.com>
2023-02-27 11:39:33 +0000

    Added tests for updated translation function

--- test_my_nuscenes_functions.py
+++ test_my_nuscenes_functions.py
@@ -5,58 +5,50 @@ import numpy as np
 from my_nuscenes_functions import find_translation, rotation, find_dist_between_ranges
 
 
-# TODO: Review all tests in this file
+def test_with_rotation(ego_bb, ann_bb, expected):
+    back = np.array(ego_bb[-1])
+    front = np.array(ego_bb[0])
+
+    ego_heading = front - back
+
+    ego_heading = np.arctan2(ego_heading[0], ego_heading[1])
+
+    for _ in range(4):
+        actual = find_translation(ego_bb, ann_bb, ego_heading)
+        np.testing.assert_allclose(expected, actual, atol=0.00001)
+        ann_bb = np.roll(ann_bb, 1, axis=0)
+
 
 class TestFindTranslation(TestCase):
     # TODO: Rewrite with updated functionality
 
     def test_1(self):
-        # Two boxes placed beside each other. 
-        ego_bb = np.array([[0, 0], [1, 0], [1, 1], [0, 1]])
-        ann_bb = np.array([[2, 0], [3, 0], [3, 1], [2, 1]])
+        ego_bb = np.array(([1, 1], [0, 1], [0, 0], [1, 0]))
+        ann_bb = np.array([[3, 1], [2, 1], [2, 0], [3, 0]])
         expected = [1, 0]
 
-        for _ in range(4):
-            actual = find_translation(ego_bb, ann_bb)
-            np.testing.assert_allclose(expected, actual)
-            ego_bb = np.roll(ego_bb, 1, axis=0)
-
-        for _ in range(4):
-            actual = find_translation(ego_bb, ann_bb)
-            np.testing.assert_allclose(expected, actual)
-            ann_bb = np.roll(ann_bb, 1, axis=0)
+        test_with_rotation(ego_bb, ann_bb, expected)
 
     def test_2(self):
-        # Two boxes placed beside each other
-        ego_bb = np.array([[0, 0], [1, 0], [1, 1], [0, 1]])
-        ann_bb = np.array([[0, 2], [1, 2], [1, 3], [0, 3]])
-        expected = [0, 1]
+        ego_bb = np.array([[3, 3], [2, 3], [2, 1], [3, 1]])
+        ann_bb = np.array([[4, 3], [6, 4], [5, 6], [3, 5]])
+        expected = [0, 0]
 
-        for _ in range(4):
-            actual = find_translation(ego_bb, ann_bb)
-            np.testing.assert_allclose(expected, actual)
-            ego_bb = np.roll(ego_bb, 1, axis=0)
-
-        for _ in range(4):
-            actual = find_translation(ego_bb, ann_bb)
-            np.testing.assert_allclose(expected, actual)
-            ann_bb = np.roll(ann_bb, 1, axis=0)
+        test_with_rotation(ego_bb, ann_bb, expected)
 
     def test_3(self):
-        # Two boxes placed diagonally from each other
-        ego_bb = np.array([[3, 3], [4, 3], [4, 4], [3, 4]])
-        ann_bb = np.array([[1, 1], [2, 1], [2, 2], [1, 2]])
-        expected = [-1, -1]
+        ego_bb = np.array([[6, 3], [6, 4], [4, 4], [4, 3]])
+        ann_bb = np.array([[2, 4], [1, 4], [1, 2], [2, 2]])
+        expected = [-2, 0]
 
-        for _ in range(4):
-            actual = find_translation(ego_bb, ann_bb)
-            np.testing.assert_allclose(expected, actual)
-            ego_bb = np.roll(ego_bb, 1, axis=0)
+        test_with_rotation(ego_bb, ann_bb, expected)
 
-        for _ in range(4):
-            actual = find_translation(ego_bb, ann_bb)
-            np.testing.assert_allclose(expected, actual)
-            ann_bb = np.roll(ann_bb, 1, axis=0)
+    def test_4(self):
+        ego_bb = np.array([[5, 6], [4, 6], [4, 4], [5, 4]])
+        ann_bb = np.array([[2, 1], [1, 2], [0, 1], [1, 0]])
+        expected = [-2, -2]
+
+        test_with_rotation(ego_bb, ann_bb, expected)
 
 
 class TestFindDistBetweenRanges(TestCase):
@@ -137,7 +129,6 @@ class TestRotation(TestCase):
         actual = rotation(angle, vector)
         np.testing.assert_allclose(expected, actual, atol=0.00001)
 
-
     def test_90_degree_4(self):
         angle = np.pi / 2
         vector = [-1, 0]
@@ -155,6 +146,6 @@ class TestRotation(TestCase):
     def test_45_degree(self):
         angle = np.pi / 4
         vector = [1, 0]
-        expected = np.array([1/math.sqrt(2), 1/math.sqrt(2)])
+        expected = np.array([1 / math.sqrt(2), 1 / math.sqrt(2)])
         actual = rotation(angle, vector)
         np.testing.assert_allclose(expected, actual, atol=0.00001)
