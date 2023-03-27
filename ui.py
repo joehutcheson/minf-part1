892c8a23f3af3d349e7e19d9408513bef12a6ad3
joe <63621767+joehutcheson@users.noreply.github.com>
2023-03-27 16:08:12 +0100

    Added whole dataset stats

--- ui.py
+++ ui.py
@@ -6,7 +6,10 @@ from threading import Timer
 from flask import Flask, render_template, redirect
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib import patches
+import seaborn as sns
 
+import constants
 
 # This fixes the issue where Matplotlib gives a threading error
 # Fixed using source:
@@ -60,6 +63,65 @@ def scene(token):
             plt.savefig(out_path)
 
     return render_template('scene.html', nusc=nusc, token=token, scores=scores)
+
+@app.route('/dataset_stats')
+def dataset_stats():
+    scores_raw = []
+    for scene in nusc.scene:
+        scores_raw += generate_scores_for_scene(nusc, scene['token'], aggressive=aggressive)
+    scores = [score['score'] for score in scores_raw]
+
+    fig, ax = plt.subplots()
+    ax.hist(scores)
+    ax.set_title('Distribution of scores over entire dataset')
+    ax.set_xlabel('Score')
+    ax.set_ylabel('Number of occurrences')
+    fig.savefig('static/temp_renders/dataset.svg')
+
+    fig, ax = plt.subplots()
+    labels = ['Perfect score', 'Non-perfect score']
+    values = [len([s for s in scores if s >= 1]), len([s for s in scores if s < 1])]
+    ax.pie(values, autopct=lambda x: int(x*len(scores)/100), labels=labels)
+    ax.set_title('Proportion of perfect to non-perfect scores')
+    fig.savefig('static/temp_renders/dataset_pie.svg')
+
+    fig, ax = plt.subplots()
+    ax.hist([s for s in scores if s < 1])
+    ax.set_title('Distribution of non-perfect scores over entire dataset')
+    ax.set_xlabel('Score')
+    ax.set_ylabel('Number of occurrences')
+    fig.savefig('static/temp_renders/dataset_danger.svg')
+
+    fig, ax = plt.subplots()
+    long = len([0 for s in scores_raw if s['reason'] == 'Longitudinally too close'])
+    lat = len([0 for s in scores_raw if s['reason'] == 'Laterally too close'])
+    other = len([0 for s in scores_raw if s['reason'] == 'Too close'])
+    reasons = ['Longitudinally too close', 'Laterally too close', 'Too close']
+    values = [long, lat, other]
+    ax.bar(reasons, values)
+    ax.set_title('Reasons given for non-perfect scores')
+    ax.set_xlabel('Reason for score')
+    ax.set_ylabel('Number of occurrences')
+    fig.savefig('static/temp_renders/dataset_reasons.svg')
+
+    # Create another graph. A heatmap. It will show the locations of other vehicles in relation to the ego
+    # where an unsafe score has been given
+
+    fig, ax = plt.subplots()
+    x = [s['lat_distance'] for s in scores_raw if s['score'] < 1]
+    y = [s['long_distance'] for s in scores_raw if s['score'] < 1]
+    ax.hist2d(y, x, [25,10], range=[[-5,50],[-10,10]])
+    l = constants.renault_zoe_dims['length']
+    w = constants.renault_zoe_dims['width']
+    rect = patches.Rectangle((-l, -w/2), l, w, linewidth=1, edgecolor='r', facecolor='w', label='Ego')
+    ax.add_patch(rect)
+    ax.set_aspect('equal')
+    ax.set_xlabel('Longitudinal distance')
+    ax.set_ylabel('Lateral distance')
+    ax.set_title('Relative translations of other vehicles in unsafe situations')
+    ax.legend()
+    fig.savefig('static/temp_renders/dataset_heatmap.svg')
+    return render_template('dataset_stats.html')
 
 
 def main():
